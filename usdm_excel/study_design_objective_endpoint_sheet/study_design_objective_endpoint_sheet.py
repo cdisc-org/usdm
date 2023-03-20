a7d87536b7eee1652007b97f9dac81f2997c3bf8
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-03-20 12:16:27 +0100

    Fix CT issue

--- usdm_excel/study_design_objective_endpoint_sheet/study_design_objective_endpoint_sheet.py
+++ usdm_excel/study_design_objective_endpoint_sheet/study_design_objective_endpoint_sheet.py
@@ -4,6 +4,7 @@ import pandas as pd
 from usdm.objective import Objective
 from usdm.endpoint import Endpoint
 from usdm_excel.cdisc_ct_library import cdisc_ct_library
+from usdm_excel.cdisc_ct import CDISCCT
 
 class StudyDesignObjectiveEndpointSheet(BaseSheet):
 
@@ -14,18 +15,22 @@ class StudyDesignObjectiveEndpointSheet(BaseSheet):
       current = None
       for index, row in self.sheet.iterrows():
         o_description = self.clean_cell(row, index, "objectiveDescription")
-        o_level = cdisc_ct_library.klass_and_attribute('StudyObjective', 'objectiveLevel', self.clean_cell(row, index, "objectiveLevel"))
+        o_level = cdisc_ct_library.klass_and_attribute('Objective', 'objectiveLevel', self.clean_cell(row, index, "objectiveLevel"))
         e_description = self.clean_cell(row, index, "endpointDescription")
         e_p_description = self.clean_cell(row, index, "endpointPurposeDescription")
-        e_level = cdisc_ct_library.klass_and_attribute('StudyObjective', 'endpointLevel', self.clean_cell(row, index, "endpointLevel"))
+        e_level = cdisc_ct_library.klass_and_attribute('Endpoint', 'endpointLevel', self.clean_cell(row, index, "endpointLevel"))
         if not o_description == "":
           current = Objective(objectiveId=self.id_manager.build_id(Objective),
             objectiveDescription=o_description, 
-            objectiveLevel=o_level,
+            objectiveLevel=CDISCCT(self.id_manager).code(code=o_level['conceptId'], decode=o_level['preferredTerm']),
             objectiveEndpoints=[]
           )
           self.objectives.append(current)
-        ep = Endpoint(endpointId=self.id_manager.build_id(Endpoint), endpointDescription=e_description, endpointPurposeDescription=e_p_description, endpointLevel=e_level)  
+        ep = Endpoint(endpointId=self.id_manager.build_id(Endpoint), 
+          endpointDescription=e_description, 
+          endpointPurposeDescription=e_p_description, 
+          endpointLevel=CDISCCT(self.id_manager).code(code=e_level['conceptId'], decode=e_level['preferredTerm'])
+        )  
         current.objectiveEndpoints.append(ep)
         
     except Exception as e:
