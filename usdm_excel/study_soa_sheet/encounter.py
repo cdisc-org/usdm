bf8ea293d4fdab303d4640327b71dc7b696dd583
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-03-29 12:40:37 +0200

    Updated to include use of xref

--- usdm_excel/study_soa_sheet/encounter.py
+++ usdm_excel/study_soa_sheet/encounter.py
@@ -2,28 +2,16 @@ from usdm_excel.base_sheet import BaseSheet
 from usdm_excel.study_soa_sheet.soa_column_rows import SoAColumnRows
 from usdm_excel.id_manager import id_manager
 from usdm.encounter import Encounter as USDMEncounter
+from usdm_excel.cross_ref import cross_references
 
 class Encounter(BaseSheet):
   
   def __init__(self, sheet, col_index):
     super().__init__(sheet)
-    #self._col_index = col_index
     self._position_key = col_index - SoAColumnRows.FIRST_VISIT_COL
-    self.name = self.clean_cell_unnamed(SoAColumnRows.VISIT_LABEL_ROW, col_index)
+    self.xref = self.clean_cell_unnamed(SoAColumnRows.VISIT_LABEL_ROW, col_index)
     self.epoch, is_null = self.clean_cell_unnamed_with_previous(SoAColumnRows.EPOCH_ROW, col_index, SoAColumnRows.FIRST_VISIT_COL)
-    self.window = self.clean_cell_unnamed(SoAColumnRows.VISIT_WINDOW_ROW, col_index)
-    self.usdm_encounter = self._as_usdm()
+    self.usdm_encounter = cross_references.get(self.xref)
 
   def key(self):
     return self._position_key
-  
-  def _as_usdm(self):
-    return USDMEncounter(
-      encounterId=id_manager.build_id(Encounter),
-      encounterName=self.name,
-      encounterDescription=self.name,
-      #encounterType: Union[Code, None] = None,
-      #encounterEnvironmentalSetting: Union[Code, None] = None,
-      #encounterContactModes: List[Code] = []
-      encounterScheduledAtTimingId=None
-    )
--- usdm_excel/study_soa_sheet/encounters.py
+++ usdm_excel/study_soa_sheet/encounters.py
@@ -1,6 +1,7 @@
 from usdm_excel.base_sheet import BaseSheet
 from usdm_excel.study_soa_sheet.soa_column_rows import SoAColumnRows
 from usdm_excel.id_manager import id_manager
+from usdm_excel.cross_ref import cross_references
 from usdm_excel.study_soa_sheet.encounter import Encounter
 
 class Encounters(BaseSheet):
@@ -14,7 +15,7 @@ class Encounters(BaseSheet):
     for col_index in range(self.sheet.shape[1]):
       if col_index >= SoAColumnRows.FIRST_VISIT_COL:
         encounter = Encounter(self.sheet, col_index)
-        if not encounter.name == "":
+        if not encounter.usdm_encounter == None:
           self.items.append(encounter)
           self._map[encounter.key()] = encounter
           #print("ENC2:", encounter)
