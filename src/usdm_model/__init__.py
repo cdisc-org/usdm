928ae6159a3c6009ece44503d92e38c89541efeb
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-03-30 21:01:42 +0200

    Improve structure

---
+++ src/usdm_model/__init__.py
@@ -0,0 +1,70 @@
+from .activity import *
+from .address import *
+from .alias_code import *
+from .analysis_population import *
+from .api_base_model import *
+from .biomedical_concept_category import *
+from .biomedical_concept_property import *
+from .biomedical_concept_surrogate import *
+from .biomedical_concept import *
+from .code import *
+from .encounter import *
+from .endpoint import *
+from .estimand import *
+from .indication import *
+from .intercurrent_event import *
+from .investigational_intervention import *
+from .objective import *
+from .organisation import *
+from .procedure import *
+from .response_code import *
+from .schedule_timeline_exit import *
+from .schedule_timeline import *
+from .scheduled_instance import *
+from .study_arm import *
+from .study_cell import *
+from .study_design_population import *
+from .study_design import *
+from .study_element import *
+from .study_epoch import *
+from .study_identifier import *
+from .study_protocol_version import *
+from .study import *
+from .timing import *
+from .transition_rule import *
+
+__all__ = [
+  'Activity',
+  'Address',
+  'AliasCode',
+  'AnalysisPopulation',
+  'BiomedicalConceptCategory',
+  'BiomedicalConceptProperty',
+  'BiomedicalConceptSurrogate',
+  'BiomedicalConcept',
+  'Code',
+  'Encounter',
+  'Endpoint',
+  'Estimand',
+  'Indication',
+  'IntercurrentEvent',
+  'InvestigationalIntervention',
+  'Objective',
+  'Organisation',
+  'Procedure',
+  'ResponseCode',
+  'ScheduleTimelineExit',
+  'ScheduleTimeline',
+  'ScheduledInstance',
+  'StudyArm',
+  'StudyCell',
+  'StudyDesignPopulation',
+  'StudyDesign',
+  'StudyElement',
+  'StudyEpoch',
+  'StudyIdentifier',
+  'StudyProtocolVersion',
+  'Study',
+  'Timing',
+  'TransitionRule',
+]
