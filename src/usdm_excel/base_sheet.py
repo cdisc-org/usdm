d698813bc363621e014cf9b733160fb64b24d4b3
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-04-03 17:58:36 +0200

    Tidy up, remove dead code

--- src/usdm_excel/base_sheet.py
+++ src/usdm_excel/base_sheet.py
@@ -19,7 +19,7 @@ class BaseSheet():
       col_index = self.sheet.columns.get_loc(field_name)
       return self.read_cell(row_index, col_index)
     except Exception as e:
-      self.error(row_index + 1, col_index, "Error (%s) reading cell row '%s', field '%s'" % (e, row_index, field_name))
+      self._error(row_index + 1, col_index, "Error (%s) reading cell row '%s', field '%s'" % (e, row_index, field_name))
       return ""
 
   def read_cell(self, row_index, col_index):
@@ -29,7 +29,7 @@ class BaseSheet():
       else:
         return str(self.sheet.iloc[row_index, col_index]).strip()
     except Exception as e:
-      self.error(row_index + 1, col_index, "Error (%s) reading cell row '%s', field '%s'" % (e, row_index, col_index))
+      self._error(row_index + 1, col_index, "Error (%s) reading cell row '%s', field '%s'" % (e, row_index, col_index))
       return ""
 
   def read_cell_multiple(self, rindex, cindex):
@@ -41,46 +41,6 @@ class BaseSheet():
       results.append(part.strip())
     return results
 
-  # def clean_cell(self, row, index, field_name):
-  #   try:
-  #     if pd.isnull(row[field_name]):
-  #       return ""
-  #     else:
-  #       return str(row[field_name]).strip()
-  #   except Exception as e:
-  #     col = self.sheet.columns.get_loc(field_name)
-  #     self.error(index + 1, col, "Cell error '%s'" % (e))
-  #     return ""
-
-  # def clean_cell_unnamed(self, rindex, cindex):
-  #   try:
-  #     if pd.isnull(self.sheet.iloc[rindex, cindex]):
-  #       return ""
-  #     else:
-  #       return str(self.sheet.iloc[rindex, cindex]).strip()
-  #   except Exception as e:
-  #     self.error(rindex + 1, cindex + 1, "Cell error '%s'" % (e))
-  #     return ""
-
-  # def read_cell_multiple(self, rindex, cindex):
-  #   results = []
-  #   value = self.read_cell(rindex, cindex)
-  #   if value.strip() == '':
-  #     return results
-  #   for part in value.split(","):
-  #     results.append(part.strip())
-  #   return results
-
-  # def clean_cell_unnamed_new(self, rindex, cindex):
-  #   try:
-  #     if pd.isnull(self.sheet.iloc[rindex, cindex]):
-  #       return "", True
-  #     else:
-  #       return self.sheet.iloc[rindex, cindex].strip(), False
-  #   except Exception as e:
-  #     self.error(rindex + 1, cindex + 1, "Cell error '%s'" % (e))
-  #     return "", True
-
   def read_cell_with_previous(self, row_index, col_index, first_col_index):
     try:
       i = col_index
@@ -89,10 +49,10 @@ class BaseSheet():
           i -= 1
         else:
           return self.sheet.iloc[row_index, i].strip()
-      self.error(row_index + 1, col_index + 1, "Blank cell error")
+      self._error(row_index + 1, col_index + 1, "Blank cell error")
       return ""
     except Exception as e:
-      self.error(row_index + 1, col_index, "Error (%s) reading cell row '%s', field '%s'" % (e, row_index, col_index))
+      self._error(row_index + 1, col_index + 1, "Error (%s) reading cell row '%s', field '%s'" % (e, row_index, col_index))
       return ""
 
   def read_boolean_cell(self, value):
@@ -108,7 +68,7 @@ class BaseSheet():
     try:
       return CDISCCT().code(code=parts[0].strip(), decode=parts[1].strip())
     except Exception as e:
-      self.error(0, 0, "CDISC code error '%s'" % (e))
+      self._error(0, 0, "CDISC code error '%s'" % (e))
       return None
 
   def read_other_code_cell_by_name(self, row_index, field_name):
@@ -119,24 +79,8 @@ class BaseSheet():
     value = self.read_cell(row_index, col_index)
     if value.strip() == "":
       return None
-    return self.decode_other_code(value)
+    return self._decode_other_code(value, row_index, col_index)
 
-  def decode_other_code(self, value):
-    if value.strip() == "":
-      return None
-    outer_parts = value.split(":")
-    if len(outer_parts) == 2:
-      system = outer_parts[0].strip()
-      inner_parts = outer_parts[1].strip().split("=")
-      if len(inner_parts) == 2:
-        version = ct_version_manager.get(system)
-        return Code(codeId=id_manager.build_id(Code), code=inner_parts[0].strip(), codeSystem=system, codeSystemVersion=version, decode=inner_parts[1].strip())
-      else:
-        self.error(0, 0, "Other code error for data '%s', no '=' detected" % (value))
-    else:
-      self.error(0, 0, "Other code error for data '%s', no ':' detected" % (value))
-    return None
-  
   def read_other_code_cell_multiple_by_name(self, row_index, field_name):
     col_index = self.sheet.columns.get_loc(field_name)
     return self.read_other_code_cell_mutiple(row_index, col_index)
@@ -148,7 +92,7 @@ class BaseSheet():
       return result
     items = value.split(",")
     for item in items:
-      code = self.decode_other_code(item.strip())
+      code = self._decode_other_code(item.strip(), row_index, col_index)
       if not code == None:
         result.append(code)
     return result
@@ -160,9 +104,6 @@ class BaseSheet():
   def read_cdisc_klass_attribute_cell(self, klass, attribute, row_index, col_index):
     value = self.read_cell(row_index, col_index)
     return CDISCCT().code_for_attribute(klass, attribute, value)
-
-  # def cdisc_klass_attribute_cell(self, klass, attribute, value):
-  #   return CDISCCT().code_for_attribute(klass, attribute, value)
 
   def read_cdisc_klass_attribute_cell_by_name_multiple(self, klass, attribute, row_index, field_name):
     col_index = self.sheet.columns.get_loc(field_name)
@@ -191,5 +132,23 @@ class BaseSheet():
         the_id = getattr(items[idx+1], id)
         setattr(item, next, the_id)
 
-  def error(self, row, column, message):
+  def _decode_other_code(self, value, row_index, col_index):
+    if value.strip() == "":
+      return None
+    outer_parts = value.split(":")
+    if len(outer_parts) == 2:
+      system = outer_parts[0].strip()
+      inner_parts = outer_parts[1].strip().split("=")
+      if len(inner_parts) == 2:
+        version = ct_version_manager.get(system)
+        return Code(codeId=id_manager.build_id(Code), code=inner_parts[0].strip(), codeSystem=system, codeSystemVersion=version, decode=inner_parts[1].strip())
+      else:
+        self._error(row_index, col_index, "Failed to decode code data '%s', no '=' detected" % (value))
+    else:
+      self._error(row_index, col_index, "Failed to decode code data '%s', no ':' detected" % (value))
+    return None
+
+  def _error(self, row, column, message):
     error_manager.add("Needed", row, column, message)
+
+
