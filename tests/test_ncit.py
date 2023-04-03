3ba144f50160d3c57d5c4bace0fd45acf731c2bb
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-04-03 18:48:58 +0200

    Add another test

---
+++ tests/test_ncit.py
@@ -0,0 +1,14 @@
+import pytest
+
+from src.usdm_excel.ncit import NCIt
+from src.usdm_excel.id_manager import id_manager
+
+def test_code():
+    id_manager.clear()
+    item = NCIt()
+    code = item.code(code="CODE", decode="DECODE")
+    assert code.codeId == "code_1"
+    assert code.code == "CODE"
+    assert code.codeSystem == "NCI Thesaurus"
+    assert code.codeSystemVersion == ""
+    assert code.decode == "DECODE"
