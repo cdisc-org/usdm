bc6453d2e383ce8219672948a3453c0bac8cb0ed
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-04-04 11:58:05 +0200

    Update test to mock open of file

--- tests/test_base_sheet.py
+++ tests/test_base_sheet.py
@@ -4,9 +4,10 @@ import pandas as pd
 from src.usdm_excel.base_sheet import BaseSheet
 
 def test_read_cell(mocker):
+    mocked_open = mocker.mock_open(read_data="File")
+    mocker.patch("builtins.open", mocked_open)
     data = {'col_1': [3, 2, 1, 0], 'col_2': ['a', 'b', 'c', 'd']}
-    mock_xxx = mocker.patch("src.usdm_excel.base_sheet.pd")
-    mock_xxx.read_excel.return_value = pd.DataFrame.from_dict(data)
-
+    mock_read = mocker.patch("pandas.read_excel")
+    mock_read.return_value = pd.DataFrame.from_dict(data)
     base = BaseSheet("", "sheet")
     assert(base.read_cell(0,0)) == "3"
