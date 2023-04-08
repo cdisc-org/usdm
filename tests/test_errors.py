98393710beed9b4edf0ad87259899ff9cacc7767
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-04-08 20:04:58 +0200

    Add further tests

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -1,5 +1,3 @@
-import pytest
-
 from src.usdm_excel.errors.errors import Errors
 
 def test_create():
@@ -18,3 +16,20 @@ def test_count():
   errors.add(sheet="My Sheet", row=1, column=99, message="XXXXX")
   errors.add(sheet="My Sheet", row=1, column=99, message="XXXXX")
   assert errors.count() == 2
+
+def test_clear():
+  errors = Errors()
+  assert len(errors.items) == 0
+  errors.add(sheet="My Sheet", row=1, column=99, message="XXXXX")
+  assert len(errors.items) == 1
+  errors.clear()
+  assert len(errors.items) == 0
+
+def test_dumo():
+  errors = Errors()
+  errors.add(sheet="My Sheet", row=1, column=99, message="XXXXX1")
+  errors.add(sheet="My Sheet", row=2, column=100, message="XXXXX2")
+  assert (errors.dump()) == [
+    {'sheet': 'My Sheet', 'row': 1, 'column': 99, 'message': 'XXXXX1', 'level': 40},
+    {'sheet': 'My Sheet', 'row': 2, 'column': 100, 'message': 'XXXXX2', 'level': 40}
+  ]
