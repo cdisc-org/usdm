b5518c3408cd0ee61338957bcb33fd960a6e093b
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-04-11 18:05:23 +0200

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name="usdm",
-  version="0.21.0",
+  version="0.22.0",
   author="D Iberson-Hurst",
   author_email="",
   description="A python package for using the CDISC TransCelerate USDM",
