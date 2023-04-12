80392c0fc1d71bfb68fd81f43c1608f5414b8760
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-04-12 19:50:49 +0200

    Amend type from Enum to string Literal

--- src/usdm_model/scheduled_instance.py
+++ src/usdm_model/scheduled_instance.py
@@ -1,16 +1,12 @@
 from enum import Enum
-from typing import List, Dict, Union
+from typing import List, Dict, Union, Literal
 
 from .api_base_model import ApiBaseModel
 from .timing import Timing
 
-class ScheduledInstanceType(Enum):
-  ACTIVITY = 1
-  DECISION = 2
-
 class ScheduledInstance(ApiBaseModel):
     scheduledInstanceId: str
-    scheduledInstanceType: ScheduledInstanceType
+    scheduledInstanceType: Literal['ACTIVITY', 'DECISION']
     scheduleSequenceNumber: int
     scheduleTimelineExitId: Union[str, None] = None
     scheduledInstanceEncounterId: Union[str, None] = None
