09c3fd53373a36b7580053f86b604e8a3e3dac7b
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-05-03 14:08:09 +0200

    Tweak test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -7,8 +7,8 @@ def run_test(filename):
     expected = json.dumps(json.load(f)) # Odd, but doing it for consistency of processing
   result = excel.to_json()
   # Useful if you want to see the results
-  with open(f"tests/integration_test_files/{filename}_result.json", 'w', encoding='utf-8') as f:
-    f.write(json.dumps(json.loads(excel.to_json()), indent=2))
+  # with open(f"tests/integration_test_files/{filename}_result.json", 'w', encoding='utf-8') as f:
+  #   f.write(json.dumps(json.loads(excel.to_json()), indent=2))
   assert result == expected
 
 def test_simple_1():
