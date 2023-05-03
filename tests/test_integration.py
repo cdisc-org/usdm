f63042c50ef8501a031d5e921c6bac2b6302466f
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-05-03 14:58:55 +0200

    Fix bug and retest

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -7,8 +7,8 @@ def run_test(filename):
     expected = json.dumps(json.load(f)) # Odd, but doing it for consistency of processing
   result = excel.to_json()
   # Useful if you want to see the results
-  # with open(f"tests/integration_test_files/{filename}_result.json", 'w', encoding='utf-8') as f:
-  #   f.write(json.dumps(json.loads(excel.to_json()), indent=2))
+  #with open(f"tests/integration_test_files/{filename}_result.json", 'w', encoding='utf-8') as f:
+  #  f.write(json.dumps(json.loads(excel.to_json()), indent=2))
   assert result == expected
 
 def test_simple_1():
@@ -16,3 +16,6 @@ def test_simple_1():
 
 def test_config_1():
   run_test('config_1')
+
+def test_config_2():
+  run_test('config_2')
