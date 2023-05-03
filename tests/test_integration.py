794638a802b93954cb9a96a02e3a8d85429dbf7f
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-05-03 08:52:01 +0200

    Fix integration tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -4,10 +4,11 @@ from src.usdm_excel import USDMExcel
 def run_test(filename):
   excel = USDMExcel(f"tests/integration_test_files/{filename}.xlsx")
   with open(f"tests/integration_test_files/{filename}.json", 'r') as f:
-    expected = "".join(line.strip() for line in f)  
+    expected = json.dumps(json.load(f)) # Odd, but doing it for consistency of processing
   result = excel.to_json()
-  with open(f"tests/integration_test_files/{filename}_result.json", 'w', encoding='utf-8') as f:
-    f.write(json.dumps(json.loads(excel.to_json()), indent=2))
+  # Useful if you want to see the results
+  # with open(f"tests/integration_test_files/{filename}_result.json", 'w', encoding='utf-8') as f:
+  #   f.write(json.dumps(json.loads(excel.to_json()), indent=2))
   assert result == expected
 
 def test_simple_1():
