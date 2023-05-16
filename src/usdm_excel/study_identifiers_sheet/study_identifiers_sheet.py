376fe2d4c207969f815813a0c6127d261bdaa67d
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-05-16 15:19:57 +0200

    Add code

--- src/usdm_excel/study_identifiers_sheet/study_identifiers_sheet.py
+++ src/usdm_excel/study_identifiers_sheet/study_identifiers_sheet.py
@@ -11,7 +11,6 @@ class StudyIdentifiersSheet(BaseSheet):
 
   def __init__(self, file_path: str):
     try:
-      #super().__init__(pd.read_excel(open(file_path, 'rb'), sheet_name='studyIdentifiers'))
       super().__init__(file_path=file_path, sheet_name='studyIdentifiers')
       self.identifiers = []
       self.process_sheet()
@@ -22,15 +21,14 @@ class StudyIdentifiersSheet(BaseSheet):
   def process_sheet(self):
     self.identifiers = []
     for index, row in self.sheet.iterrows():
-      organisation_type = self.read_cdisc_klass_attribute_cell_by_name('Organisation', 'organisationType', index, 'organisationType')
-      raw_address=self.read_cell_by_name(index, 'organisationAddress')
+      organisation_type = self.read_cdisc_klass_attribute_cell_by_name('Organisation', 'organisationType', index, 'organisationType')     
       organisation = Organisation(
         organisationId=id_manager.build_id(Organisation),
         organisationIdentifierScheme=self.read_cell_by_name(index, 'organisationIdentifierScheme'), 
         organisationIdentifier=self.read_cell_by_name(index, 'organisationIdentifier'),
         organisationName=self.read_cell_by_name(index, 'organisationName'),
         organisationType=organisation_type,
-        organizationLegalAddress=self._build_address(raw_address)
+        organizationLegalAddress=self._build_address(index)
       )
       self.identifiers.append(StudyIdentifier(
         studyIdentifierId=id_manager.build_id(StudyIdentifier),
@@ -38,22 +36,34 @@ class StudyIdentifiersSheet(BaseSheet):
         studyIdentifierScope=organisation)
       )
     
-  def _build_address(self, raw_address):
-    parts = raw_address.split("|")
-    if len(parts) == 6:
-      return self._to_address(
-            id_manager.build_id(Address),
-            parts[0].strip(), 
-            parts[1].strip(), 
-            parts[2].strip(), 
-            parts[3].strip(), 
-            parts[4].strip(), 
-            ISO3166().code(parts[5].strip())
-          )
+  def _build_address(self, row_index):
+    field_name = 'organisationAddress'
+    raw_address = self.read_cell_by_name(row_index, field_name)
+    # The '|' separator is preserved for legacy reasons but should be removed in the future
+    if '|' in raw_address:
+      sep = '|'
+      parts = raw_address.split(sep)
     else:
+      sep = ','
+      parts = self._state_split(raw_address)
+    if len(parts) == 6:
+      result = self._to_address(
+          id_manager.build_id(Address),
+          parts[0].strip(), 
+          parts[1].strip(), 
+          parts[2].strip(), 
+          parts[3].strip(), 
+          parts[4].strip(), 
+          ISO3166().code(parts[5].strip())
+        )
+      return result
+    else:
+      col_index = self.sheet.columns.get_loc(field_name)
+      self._error(row_index, col_index, f"Address does not contain the required fields (line, city, district, state, postal code and country code) using '{sep}' separator characters, only {len(parts)} found")
       return None
 
   def _to_address(self, id, line, city, district, state, postal_code, country):
     text = "%s, %s, %s, %s, %s, %s" % (line, city, district, state, postal_code, country.decode)
     text = text.replace(' ,', '')
-    return Address(addressId=id, text=text, line=line, city=city, district=district, state=state, postalCode=postal_code, country=country)
+    result = Address(addressId=id, text=text, line=line, city=city, district=district, state=state, postalCode=postal_code, country=country)
+    return result
