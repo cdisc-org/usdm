740199c20f2774eb4438719d92cfbac2b0bdacc6
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-06-05 17:02:32 +0200

    Integer constraint update

--- src/usdm_model/study_design_population.py
+++ src/usdm_model/study_design_population.py
@@ -1,11 +1,12 @@
-from .api_base_model import ApiBaseModel
+from pydantic import NonNegativeInt
 from typing import List
+from .api_base_model import ApiBaseModel
 from .code import Code
 
 class StudyDesignPopulation(ApiBaseModel):
   studyDesignPopulationId: str
   populationDescription: str
-  plannedNumberOfParticipants: int
+  plannedNumberOfParticipants: NonNegativeInt
   plannedMaximumAgeOfParticipants: str  
   plannedMinimumAgeOfParticipants: str
   plannedSexOfParticipants: List[Code] = []
