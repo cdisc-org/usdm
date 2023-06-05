62491992385275ce8f0f64cb55587f7b514784ea
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-06-05 16:44:06 +0200

    Update BC Category

--- src/usdm_model/biomedical_concept_category.py
+++ src/usdm_model/biomedical_concept_category.py
@@ -3,8 +3,7 @@ from .api_base_model import ApiBaseModel
 
 class BiomedicalConceptCategory(ApiBaseModel):
   biomedicalConceptCategoryId: str
-  bcCategoryParentIds: List[str] = []
-  bcCategoryChildrenIds: List[str] = []
+  bcCategoryChildIds: List[str] = []
   bcCategoryName: str
   bcCategoryDescription: str
   bcCategoryMemberIds: List[str] = []
