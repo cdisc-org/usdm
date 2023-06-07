196ab749c0d59b89fe1f52c6868ff9677acb7864
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-06-07 15:29:47 +0200

    Fixes

--- src/usdm_excel/iso_8601_duration.py
+++ src/usdm_excel/iso_8601_duration.py
@@ -8,17 +8,19 @@ class ISO8601Duration():
   def encode(self, value, unit):
     the_value = int(value.strip())
     unit_text = unit.strip()
-    if unit_text.upper() in ['YRS', 'Y', 'YEARS']:
+    if unit_text.upper() in ['Y', 'YRS', 'YR', 'YEARS', 'YEAR']:
       return f"P{the_value}Y"
-    if unit_text.upper() in ['MTHS', 'MTH', 'MONTHS']:
+    if unit_text.upper() in ['MTHS', 'MTH', 'MONTHS', 'MONTH']:
       return f"P{the_value}M"
-    if unit_text.upper() in ['D', 'DAYS']:
+    if unit_text.upper() in ['W', 'WKS', 'WK', 'WEEKS', 'WEEK']:
+      return f"P{the_value}W"
+    if unit_text.upper() in ['D', 'DYS', 'DY', 'DAYS', 'DAY']:
       return f"P{the_value}D"
-    if unit_text.upper() in ['H', 'HRS', 'HOURS']:
+    if unit_text.upper() in ['H', 'HRS', 'HR', 'HOURS', 'HOUR']:
       return f"PT{the_value}H"
-    if unit_text.upper() in ['M', 'MINS', 'MINUTES']:
+    if unit_text.upper() in ['M', 'MINS', 'MIN', 'MINUTES', 'MINUTE']:
       return f"PT{the_value}M"
-    if unit_text.upper() in ['S', 'SECS', 'SECONDS']:
+    if unit_text.upper() in ['S', 'SECS', 'SEC', 'SECONDS', 'SECOND']:
       return f"PT{the_value}S"
     raise ValueError
   
