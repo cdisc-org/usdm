eeea1cdba7b21d517adf22c4e51d951afb0367ab
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-06-12 07:36:31 +0200

    Add simple Arms and Epochs test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,27 +1,29 @@
 import json
 from src.usdm_excel import USDMExcel
 
-def run_test(filename):
+def run_test(filename, save=False):
   excel = USDMExcel(f"tests/integration_test_files/{filename}.xlsx")
   result = excel.to_json()
 
-  # Useful if you want to see the results. Normally comment out
-  #with open(f"tests/integration_test_files/{filename}.json", 'w', encoding='utf-8') as f:
-  #  f.write(json.dumps(json.loads(result), indent=2))
+  # Useful if you want to see the results.
+  if save:
+    with open(f"tests/integration_test_files/{filename}.json", 'w', encoding='utf-8') as f:
+      f.write(json.dumps(json.loads(result), indent=2))
   
   with open(f"tests/integration_test_files/{filename}.json", 'r') as f:
     expected = json.dumps(json.load(f)) # Odd, but doing it for consistency of processing
   assert result == expected
 
-def run_test_ne(filename):
+def run_test_ne(filename, save=False):
   result = {}
   excel = USDMExcel(f"tests/integration_test_files/{filename}.xlsx")
   result['n'], result['e'] = excel.to_nodes_and_edges()
   for type in ['n', 'e']:
 
-    # Useful if you want to see the results. Normally comment out
-    #with open(f"tests/integration_test_files/{filename}_{type}.json", 'w', encoding='utf-8') as f:
-    #  f.write(json.dumps(result[type], indent=2))
+    # Useful if you want to see the results.
+    if save:
+      with open(f"tests/integration_test_files/{filename}_{type}.json", 'w', encoding='utf-8') as f:
+        f.write(json.dumps(result[type], indent=2))
     
     with open(f"tests/integration_test_files/{filename}_{type}.json", 'r') as f:
       expected = json.load(f)
@@ -50,3 +52,9 @@ def test_complex_1():
 
 def test_complex_1_ne():
   run_test_ne('complex_1')
+
+def test_complex_1():
+  run_test('arms_epochs', True)
+
+def test_complex_1_ne():
+  run_test_ne('arms_epochs', True)
