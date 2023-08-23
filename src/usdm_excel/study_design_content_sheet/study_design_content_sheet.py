fa75aad344077ba6cfb2cc6c76de7da2cf1b14d1
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-08-23 16:27:44 +0200

    Tweak for optional sheet

--- src/usdm_excel/study_design_content_sheet/study_design_content_sheet.py
+++ src/usdm_excel/study_design_content_sheet/study_design_content_sheet.py
@@ -10,75 +10,76 @@ class StudyDesignContentSheet(BaseSheet):
 
   def __init__(self, file_path):
     try:
+      self.items = []
       first_level = 1 
       last_level = self.__class__.SECTION_LEVELS + 1
       converters = {}
       for level in range(first_level, last_level):
         converters[self._level_column_name(level)] = str
       super().__init__(file_path=file_path, sheet_name='studyDesignContent', optional=True, converters=converters)
-      self.items = []
-      current_level = 0
-      new_level = 0
-      current_parent = []
-      previous_item = Content(
-        id=id_manager.build_id(Content), 
-        name="ROOT",
-        sectionNumber="0",
-        sectionTitle="Root",
-        text="",
-        contentChildIds=[]
-      )
-      self.items.append(previous_item)
-      for index, row in self.sheet.iterrows():
-        for level in range(first_level, last_level):
-          number = str(self.read_cell_by_name(index, self._level_column_name(level)))
-          if number != "":
-            new_level = level
-            break
-        title = self.read_cell_by_name(index, 'sectionTitle')
-        text = self.read_cell_by_name(index, 'text')
-        name = self.read_cell_by_name(index, 'name')
-        name = f"SECTION {number}" if name == "" else name
-        #print(f"PARAMS: New={new_level}, Current={current_level}, Num={number}, Title={title}, Text={text}, Name={name}")
-        try:
-          item = Content(
-            id=id_manager.build_id(Content), 
-            name=name,
-            sectionNumber=number,
-            sectionTitle=title,
-            text=text,
-            contentChildIds=[]
-          )
-          self.items.append(item)
-          #print(f"ITEM: {item.id}")
-          if new_level == current_level:
-            # Same level
-            parent = current_parent[-1]
-            #print(f"PARENT1: P={parent.id}, C={item.id}")
-            parent.contentChildIds.append(item.id)
-          elif new_level > current_level:
-            # Down
-            current_parent.append(previous_item)
-            current_level = new_level
-            parent = current_parent[-1]
-            #print(f"PARENT2: P={parent.id}, C={item.id} ")
-            parent.contentChildIds.append(item.id)
-          else:
-            # Up
-            for p_count in range(new_level, current_level):
-              #print(f"POP")
-              current_parent.pop()
-            parent = current_parent[-1]
-            #print(f"PARENT3: P={parent.id}, C={item.id}")
-            parent.contentChildIds.append(item.id)
-            current_level = new_level
-          previous_item = item
-          #print("")
-          #print("")
-        except Exception as e:
-          self._general_error(f"Failed to create Content object, exception {e}")
-          #print(f"{traceback.format_exc()}")
-          self._traceback(f"{traceback.format_exc()}")
+      if self.sheet is not None:
+        current_level = 0
+        new_level = 0
+        current_parent = []
+        previous_item = Content(
+          id=id_manager.build_id(Content), 
+          name="ROOT",
+          sectionNumber="0",
+          sectionTitle="Root",
+          text="",
+          contentChildIds=[]
+        )
+        self.items.append(previous_item)
+        for index, row in self.sheet.iterrows():
+          for level in range(first_level, last_level):
+            number = str(self.read_cell_by_name(index, self._level_column_name(level)))
+            if number != "":
+              new_level = level
+              break
+          title = self.read_cell_by_name(index, 'sectionTitle')
+          text = self.read_cell_by_name(index, 'text')
+          name = self.read_cell_by_name(index, 'name')
+          name = f"SECTION {number}" if name == "" else name
+          #print(f"PARAMS: New={new_level}, Current={current_level}, Num={number}, Title={title}, Text={text}, Name={name}")
+          try:
+            item = Content(
+              id=id_manager.build_id(Content), 
+              name=name,
+              sectionNumber=number,
+              sectionTitle=title,
+              text=text,
+              contentChildIds=[]
+            )
+            self.items.append(item)
+            #print(f"ITEM: {item.id}")
+            if new_level == current_level:
+              # Same level
+              parent = current_parent[-1]
+              #print(f"PARENT1: P={parent.id}, C={item.id}")
+              parent.contentChildIds.append(item.id)
+            elif new_level > current_level:
+              # Down
+              current_parent.append(previous_item)
+              current_level = new_level
+              parent = current_parent[-1]
+              #print(f"PARENT2: P={parent.id}, C={item.id} ")
+              parent.contentChildIds.append(item.id)
+            else:
+              # Up
+              for p_count in range(new_level, current_level):
+                #print(f"POP")
+                current_parent.pop()
+              parent = current_parent[-1]
+              #print(f"PARENT3: P={parent.id}, C={item.id}")
+              parent.contentChildIds.append(item.id)
+              current_level = new_level
+            previous_item = item
+            #print("")
+            #print("")
+          except Exception as e:
+            self._general_error(f"Failed to create Content object, exception {e}")
+            #print(f"{traceback.format_exc()}")
+            self._traceback(f"{traceback.format_exc()}")
     except Exception as e:
       self._general_error(f"Exception [{e}] raised reading sheet.")
       #print(f"{traceback.format_exc()}")
--- tests/test_study_design_content_sheet.py
+++ tests/test_study_design_content_sheet.py
@@ -6,6 +6,8 @@ xfail = pytest.mark.xfail
 from src.usdm_excel.study_design_content_sheet.study_design_content_sheet import StudyDesignContentSheet
 
 def test_create(mocker):
+  mock_present = mocker.patch("usdm_excel.base_sheet.BaseSheet._sheet_present")
+  mock_present.side_effect=[True]
   mock_id = mocker.patch("usdm_excel.id_manager.build_id")
   mock_id.side_effect=['Content_1', 'Content_2', 'Content_3', 'Content_4', 'Content_5', 'Content_6', 'Content_7']
   mocked_open = mocker.mock_open(read_data="File")
@@ -23,14 +25,17 @@ def test_create(mocker):
   mock_read.return_value = pd.DataFrame(data, columns=['sectionNumber1', 'sectionNumber2', 'sectionNumber3', 'name', 'sectionTitle', 'text'])
   content = StudyDesignContentSheet("")
   assert len(content.items) == 7
-  assert content.items[0].id == 'Content_1'
-  assert content.items[0].name == 'SECTION 1'
-  assert content.items[0].sectionNumber == '1'
-  assert content.items[0].sectionTitle == 'Section 1'
-  assert content.items[0].text == 'Text 1'
-  assert content.items[0].contentChildIds == ['Content_2', 'Content_3']
+  assert content.items[0].name == 'ROOT'
+  assert content.items[1].id == 'Content_2'
+  assert content.items[1].name == 'SECTION 1'
+  assert content.items[1].sectionNumber == '1'
+  assert content.items[1].sectionTitle == 'Section 1'
+  assert content.items[1].text == 'Text 1'
+  assert content.items[1].contentChildIds == ['Content_3', 'Content_4']
   
 def test_create_empty(mocker):
+  mock_present = mocker.patch("usdm_excel.base_sheet.BaseSheet._sheet_present")
+  mock_present.side_effect=[True]
   mocked_open = mocker.mock_open(read_data="File")
   mocker.patch("builtins.open", mocked_open)
   data = []
@@ -40,6 +45,8 @@ def test_create_empty(mocker):
   assert len(content.items) == 0
 
 def test_read_cell_by_name_error(mocker):
+  mock_present = mocker.patch("usdm_excel.base_sheet.BaseSheet._sheet_present")
+  mock_present.side_effect=[True]
   mock_error = mocker.patch("usdm_excel.errors.errors.Errors.add")
   mocked_open = mocker.mock_open(read_data="File")
   mocker.patch("builtins.open", mocked_open)
