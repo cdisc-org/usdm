68d46135d75de79a52a12d31dbcbd829199edf65
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-08-24 14:47:43 +0200

    Further configuration updates

--- tests/test_configuration_sheet.py
+++ tests/test_configuration_sheet.py
@@ -15,20 +15,18 @@ def test_defaults(mocker):
   assert om.get(Options.ROOT) == RootOption.API_COMPLIANT.value
   assert om.get(Options.PREVIOUS_NEXT) == PrevNextOption.NONE.value
   assert om.get(Options.DESCRIPTION) == ""
-  assert om.get(Options.CONTENT_LEVELS) == '3'
 
 def test_set(mocker):
   mock_error = mocker.patch("usdm_excel.errors.errors.Errors.add")
   mocked_open = mocker.mock_open(read_data="File")
   mocker.patch("builtins.open", mocked_open)
-  data = {'col_1': ['Ct Version', 'SDR Prev Next', 'sdr ROOT', 'sdr Description', 'Content levels'], 'col_2': ['THIS=that', 'sdr', 'SDR', 'No desc set', '4']}
+  data = {'col_1': ['Ct Version', 'SDR Prev Next', 'sdr ROOT', 'sdr Description'], 'col_2': ['THIS=that', 'sdr', 'SDR', 'No desc set']}
   mock_read = mocker.patch("pandas.read_excel")
   mock_read.return_value = pd.DataFrame(data)
   configuration = ConfigurationSheet("")
   assert om.get(Options.ROOT) == RootOption.API_COMPLIANT.value
   assert om.get(Options.PREVIOUS_NEXT) == PrevNextOption.NULL_STRING.value
   assert om.get(Options.DESCRIPTION) == "No desc set"
-  assert om.get(Options.CONTENT_LEVELS) == '4'
   assert ctvm.get('THIS') == 'that'
   mock_error.assert_called()
   assert mock_error.call_args[0][0] == "configuration"
