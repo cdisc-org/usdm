8d9545d8581e153828ae952662fa1f1487833157
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-08-26 14:27:21 +0200

    Update cross references for identifier map

--- src/usdm_excel/cross_ref.py
+++ src/usdm_excel/cross_ref.py
@@ -2,27 +2,38 @@ class CrossRef():
 
   def __init__(self):
     self.references = {}
+    self.identifiers = {}
 
   def clear(self):
     self.references = {}
+    self.identifiers = {}
 
   def add(self, name, object):
     klass = object.__class__
-    key = self._key(klass, name)
+    key, id_key = self._key(klass, name, object.id)
     if not key in self.references:
       self.references[key] = object
+      self.identifiers[id_key] = object
     else:
       pass # Need exception here
 
   def get(self, klass, name):
-    key = self._key(klass, name)
+    key, id_key = self._key(klass, name, "")
     if key in self.references:
       return self.references[key]
     else:
       return None
 
-  def _key(self, klass, name):
+  def get_by_id(self, klass, id):
+    key, id_key = self._key(klass, "", id)
+    if id_key in self.identifiers:
+      return self.identifiers[id_key]
+    else:
+      return None
+
+  def _key(self, klass, name, id):
     key = f"{klass.__name__}.{name}"
-    return key
+    id_key = f"{klass.__name__}.{id}"
+    return key, id_key
   
 cross_references = CrossRef()
--- tests/test_cross_refernce.py
+++ tests/test_cross_refernce.py
@@ -17,6 +17,8 @@ def test_add():
   cross_references.add("name", item)
   assert len(cross_references.references.keys()) == 1
   assert cross_references.references["CRTest.name"] == item
+  assert len(cross_references.identifiers.keys()) == 1
+  assert cross_references.identifiers["CRTest.1234"] == item
 
 def test_get():
   item = CRTest(id="1234", name="name")
@@ -24,10 +26,20 @@ def test_get():
   cross_references.references["CRTest.name"] = item
   assert cross_references.get(CRTest, "name") == item
 
+def test_get_by_id():
+  item = CRTest(id="1234", name="name")
+  cross_references.identifiers = {}
+  cross_references.identifiers["CRTest.1234"] = item
+  assert cross_references.get_by_id(CRTest, "1234") == item
+
 def test_clear():
   item = CRTest(id="1234", name="name")
   cross_references.references = {}
+  cross_references.identifiers = {}
   cross_references.references["CRTest.name"] = item
+  cross_references.identifiers["CRTest.1234"] = item
   assert len(cross_references.references.keys()) == 1
+  assert len(cross_references.identifiers.keys()) == 1
   cross_references.clear()
   assert len(cross_references.references.keys()) == 0
+  assert len(cross_references.identifiers.keys()) == 0
