617921885b52671d1c62fe9217c54ab91b499e85
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-11-23 13:59:48 +0100

    Add range class and tests

---
+++ src/usdm_excel/range_type.py
@@ -0,0 +1,29 @@
+import re
+from usdm_excel.cdisc_ct import CDISCCT
+
+class RangeType():
+
+  def __init__(self, range_info):
+    try:
+      self.upper = None
+      self.lower = None
+      self.units = None
+      self.errors = []
+      self.label = range_info.strip()
+      if range_info:
+        match = re.match(r"(?P<lower>[+|-]*\d+)\s*\.\.\s*(?P<upper>[+|-]*\d+) \s*(?P<units>.+)", self.label)
+        if match :
+          parts = match.groupdict()
+          self.lower = parts['lower'].strip()
+          self.upper = parts['upper'].strip()
+          self.units = parts['units'].strip()
+          self.units_code = CDISCCT().code_for_unit(self.units)
+          if not self.units_code:        
+            self.errors.append(f"Unable to set the units code for the range '{range_info}'")
+        else:
+          self.errors.append(f"Could not decode the range value, not all required parts detected in '{range_info}'")
+      else:
+        self.errors.append(f"Could not decode the range value, appears to be empty '{range_info}'")
+    except Exception as e:
+      self.errors.append(f"Exception [{e}] raised decoding range '{range_info}")
+
---
+++ tests/test_range_type.py
@@ -0,0 +1,28 @@
+from usdm_excel.range_type import RangeType
+
+def test_range_type(mocker):
+  test_data = [
+    ('1..1 Days', "1", "1", '1..1 Days', 'Days', 'C25301'),
+    (' -1..1 days', "-1", "1", '-1..1 days', 'days', 'C25301'),
+    ('5 .. 10 weeks ', "5", "10", '5 .. 10 weeks', 'weeks', 'C29844'),
+  ]
+  for index, test in enumerate(test_data):
+    item = RangeType(test[0])
+    assert(item.lower) == test[1]
+    assert(item.upper) == test[2]
+    assert(item.label) == test[3]
+    assert(item.units) == test[4]
+    assert(item.units_code.code) == test[5]
+    assert(item.errors) == [] 
+
+def test_range_type_error(mocker):
+  test_data = [
+    ('1.. Days',"Could not decode the range value, not all required parts detected in '1.. Days'"),
+    ('-1.1 days',"Could not decode the range value, not all required parts detected in '-1.1 days'"),
+    ('-1 .. 1',"Could not decode the range value, not all required parts detected in '-1 .. 1'"),
+    (' .. 1 Weeks',"Could not decode the range value, not all required parts detected in ' .. 1 Weeks'"),
+    ('1 .. 10 slugs',"Unable to set the units code for the range '1 .. 10 slugs'")
+  ]
+  for index, test in enumerate(test_data):
+    item = RangeType(test[0])
+    assert item.errors == [test[1]]
