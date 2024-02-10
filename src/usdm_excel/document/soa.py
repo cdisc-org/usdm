98bc12b36bb0f5223f4ac084851f7558b94f153c
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2024-02-10 16:55:02 +0100

    Initial run, not working

--- src/usdm_excel/document/soa.py
+++ src/usdm_excel/document/soa.py
@@ -1,7 +1,9 @@
-#from usdm_excel.cross_ref import cross_references
+from usdm_excel.cross_ref import cross_references
 from usdm_excel.base_sheet import BaseSheet
 #from usdm_excel.study_sheet.study_sheet import Study
 from usdm_model.schedule_timeline import ScheduleTimeline
+from usdm_model.activity import Activity
+from usdm_model.scheduled_instance import ScheduledActivityInstance, ScheduledDecisionInstance
 from usdm_model.study_design import StudyDesign
 
 class SoA():
@@ -12,32 +14,38 @@ class SoA():
     self.timeline = timeline
 
   def _first(self, collection):
-    return next((item for item in collection if not item.previous), None)
+    return next((item for item in collection if not item.previousId), None)
 
-  def _find_sai(self, id):
-    return next((item for item in self.timeline if item.id == id), None)
+  def _find(self, collection, id):
+    return next((item for item in collection if item.id == id), None)
 
   def simple(self):
 
     # Activity order
     activity_order = []
-    next = self._first(self.study_design.activities)
+    item = self._first(self.study_design.activities)
     more = True
+    print("1")
     while more:
-      activity_order.append(next)
-      more = False if not next.nextId else True
+      activity_order.append(item)
+      print("2")
+      more = False if not item.nextId else True
+      item = self._find(self.study_design.activities, item.nextId)
     print(f"ACTIVITY ORDER: {activity_order}")
     print("")
     print("")
 
     # ScheduleActivityInstance order
     sai_order = []
-    next = self._find_sai(self.timeline.entryId)
+    timeline = self.timeline
+    item = self._find(timeline.instances, self.timeline.entryId)
     more = True
+    print("3")
     while more:
       sai_order.append(next)
-      more = False if not next.nextId else True
-      next = self._find_sai(next.defaultConditionId)
+      print("4")
+      more = False if not item.defaultConditionId else True
+      item = self._find(timeline.instances, item.defaultConditionId)
     print(f"SAI ORDER: {sai_order}")
     print("")
     print("")
--- tests/test_document_soa.py
+++ tests/test_document_soa.py
@@ -1,28 +1,46 @@
-#from src.usdm_excel.cross_ref import cross_references
-#from src.usdm_excel.id_manager import id_manager
-from src.usdm_excel.base_sheet import BaseSheet
-from src.usdm_excel.cdisc_ct import CDISCCT
-from src.usdm_excel.document.soa import SoA
-from src.usdm_model.activity import Activity
-from src.usdm_model.study_epoch import StudyEpoch
-from src.usdm_model.encounter import Encounter
-from src.usdm_model.study_design import StudyDesign
-from src.usdm_model.schedule_timeline import ScheduleTimeline
+#from usdm_excel.cross_ref import cross_references
+#from usdm_excel.id_manager import id_manager
+import pandas as pd
+from usdm_excel.base_sheet import BaseSheet
+from usdm_excel.cdisc_ct import CDISCCT
+from usdm_excel.document.soa import SoA
+from usdm_model.activity import Activity
+from usdm_model.study_epoch import StudyEpoch
+from usdm_model.study_cell import StudyCell
+from usdm_model.study_arm import StudyArm
+from usdm_model.encounter import Encounter
+from usdm_model.study_design import StudyDesign
+from usdm_model.schedule_timeline import ScheduleTimeline
+from usdm_model.scheduled_instance import ScheduledActivityInstance
+from usdm_model.schedule_timeline_exit import ScheduleTimelineExit
 
 from tests.test_factory import Factory
 
-bs = BaseSheet
 cdisc_ct = CDISCCT()
 factory = Factory()
 
 dummy_code = cdisc_ct.code("C12345", "decode")
 
+def double_link(items, prev, next):
+  for idx, item in enumerate(items):
+    if idx == 0:
+      setattr(item, prev, None)
+    else:
+      the_id = getattr(items[idx-1], 'id')
+      setattr(item, prev, the_id)
+    if idx == len(items)-1:  
+      setattr(item, next, None)
+    else:
+      the_id = getattr(items[idx+1], 'id')
+      setattr(item, next, the_id)
+
 def create_activities():
   item_list = [
-    {'name': 'A1', 'label': '', 'description': '', 'definedProcedures': [], 'biomedicalConceptIds': [], 'bcCategoryIds': [], 'bcSurrogateIds': [], 'timelineId': None}
+    {'name': 'A1', 'label': '', 'description': '', 'definedProcedures': [], 'biomedicalConceptIds': [], 'bcCategoryIds': [], 'bcSurrogateIds': [], 'timelineId': None},
+    {'name': 'A2', 'label': '', 'description': '', 'definedProcedures': [], 'biomedicalConceptIds': [], 'bcCategoryIds': [], 'bcSurrogateIds': [], 'timelineId': None}
   ]
   results = factory.set(Activity, item_list)
-  bs.double_link(results, 'previousId', 'nextId')
+  double_link(results, 'previousId', 'nextId')
   return results
 
 def create_epochs():
@@ -30,7 +48,7 @@ def create_epochs():
     {'name': 'EP1', 'label': '', 'description': '', 'type': dummy_code},
   ]
   results = factory.set(StudyEpoch, item_list)
-  bs.double_link(results, 'previousId', 'nextId')
+  double_link(results, 'previousId', 'nextId')
   return results
 
 def create_encounters():
@@ -38,26 +56,45 @@ def create_encounters():
     {'name': 'E1', 'label': '', 'description': '', 'type': dummy_code, 'environmentalSetting': [], 'contactModes': [], 'transitionStartRule': None, 'transitionEndRule': None, 'scheduledAtId': None},
   ]
   results = factory.set(Encounter, item_list)
-  bs.double_link(results, 'previousId', 'nextId')
+  double_link(results, 'previousId', 'nextId')
   return results
 
-def create_activity_instance():
+def create_activity_instances():
   item_list = [
+    {'timelineExitId': None, 'encounterId': None, 'scheduledInstanceTimelineId': None, 'defaultConditionId': None, 'epochId': None, 'activityIds': []},
+    {'timelineExitId': None, 'encounterId': None, 'scheduledInstanceTimelineId': None, 'defaultConditionId': None, 'epochId': None, 'activityIds': []},
     {'timelineExitId': None, 'encounterId': None, 'scheduledInstanceTimelineId': None, 'defaultConditionId': None, 'epochId': None, 'activityIds': []}
   ]
-  results = factory.set(Encounter, item_list)
+  results = factory.set(ScheduledActivityInstance, item_list)
   return results
 
 def scenario_1():
+  dummy_cell = factory.item(StudyCell, {'armId': "X", 'epochId': "Y"})
+  dummy_arm = factory.item(StudyArm, {'name': "Arm1", 'type': dummy_code, 'dataOriginDescription': 'xxx', 'dataOriginType': dummy_code})
   activities = create_activities()
   epochs = create_epochs()
   encounters = create_encounters()
-  activity_instances = create_activity_instance()
-  study_design = factory.item(StudyDesign, {'name': 'Study Design', 'label': '', 'description': ''})
-  timeline = factory.item(ScheduleTimeline, {'name': 'Study Design', 'label': '', 'description': '', 'mainTimeline': True, 'entryCondition': None, 'entryId': activity_instances[0], 'exits': exit, 'instances': activity_instances})
+  activity_instances = create_activity_instances()
+  print(f"AI: {activity_instances}")
+  exit = factory.item(ScheduleTimelineExit, {})
+  activity_instances[-1].timelineExitId = exit.id
+  timeline = factory.item(ScheduleTimeline, {'name': 'Study Design', 'label': '', 'description': '', 'mainTimeline': True, 'entryCondition': "Condition", 'entryId': activity_instances[0].id, 'exits': [exit], 'instances': activity_instances})
+  study_design = factory.item(StudyDesign, {'name': 'Study Design', 'label': '', 'description': '', 
+    'rationale': 'XXX', 'interventionModel': dummy_code,
+    'arms': [dummy_arm], 'studyCells': [dummy_cell], 'epochs': epochs,
+    'activities': activities, 'scheduledTimelines': [timeline]})
   return study_design, timeline
 
-def test_create():
+def fake_sheet(mocker):
+  mocked_open = mocker.mock_open(read_data="File")
+  mocker.patch("builtins.open", mocked_open)
+  data = []
+  mock_read = mocker.patch("pandas.read_excel")
+  mock_read.return_value = pd.DataFrame(data, columns=[])
+  return BaseSheet("", "")
+
+def test_create(mocker):
+  bs = fake_sheet(mocker)
   study_design, timeline = scenario_1()
   soa = SoA(bs, study_design, timeline)
   result = soa.simple()
