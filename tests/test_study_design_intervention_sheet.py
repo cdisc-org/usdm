2e6bf71b635a5b624c9775523400458a3fab59fd
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2024-02-16 13:12:32 +0100

    Fix test

--- tests/test_study_design_intervention_sheet.py
+++ tests/test_study_design_intervention_sheet.py
@@ -19,6 +19,9 @@ def read_json(filename):
     data = json.load(f)
   return data
 
+def dump_json(data):
+  print(f"\n{json.dumps(data, indent=4)}")
+
 def test_create(mocker):
   clear()
   expected = read_json(f"tests/integration_test_files/intervention/create.json")
@@ -31,16 +34,21 @@ def test_create(mocker):
   mocked_open = mocker.mock_open(read_data="File")
   mocker.patch("builtins.open", mocked_open)
   data = [
-    # name     description    label          codes                          role                type      pharmacologicalClass  productDesignation      minimumResponseDuration,  administrationName [10] administrationDescription administrationLabel administrationRoute administrationDose administrationFrequency administrationDurationDescription administrationDurationWillVary administrationDurationWillVaryReason administrationDurationQuantity
-    [ 'Int 1', 'Int Desc 1',  'Int Label 1', 'SPONSOR: A=B',                'M11: role1=role1', 'C12345', 'FDA: A=B',           'M11: desig1=desig_1',  '1 Day',                  'Admin 1',              'Admin Desc 1',           'Admin Label 1',    'C34567',          '12 mg',            'C65432',               'Dur desc 1',                     'False',                       '',                                  '14 %'                         ], 
-    [ '',      '',            '',            '',                            '',                 '',       '',                   '',                     '',                       'Admin 2',              'Admin Desc 2',           'Admin Label 1',    'C34567',          '1 mg',             'C65432',               'Dur desc 1',                     'False',                       '',                                  '10 m'                         ], 
-    [ 'Int 2', 'Int Desc 2',  'Int Label 2', 'SPONSOR: C=D',                'M11: role2=role2', 'C12345', 'FDA: A=B',           'M11: desig2=desig_2',  '3 Weeks',                'Admin 3',              'Admin Desc 3',           'Admin Label 1',    'C34567',          '100 mg',           'C65432',               'Dur desc 1',                     'False',                       '',                                  '12 C'                         ], 
-    [ 'Int 3', 'Int Desc 3',  'Int Label 3', 'SPONSOR: E=F, SPONSOR: G=H',  'M11: role3=role3', 'C12345', 'FDA: A=B',           'M11: desig3=desig_3',  '4 Years',                'Admin 4',              'Admin Desc 4',           'Admin Label 1',    'C34567',          '500 mg',           'C65432',               'Dur desc 1',                     'False',                       '',                                  '12 F'                         ], 
-    [ '',      '',            '',            '',                            '',                 '',       '',                   '',                     '',                       'Admin 5',              'Admin Desc 5',           'Admin Label 1',    'C34567',          '1 mg',             'C65432',               'Dur desc 1',                     'False',                       '',                                  '15 in'                        ], 
+    # name     description    label          codes                          role                         type      pharmacologicalClass  productDesignation      minimumResponseDuration,  administrationName [10] administrationDescription administrationLabel administrationRoute administrationDose administrationFrequency administrationDurationDescription administrationDurationWillVary administrationDurationWillVaryReason administrationDurationQuantity
+    [ 'Int 1', 'Int Desc 1',  'Int Label 1', 'SPONSOR: A=B',                'Experimental Intervention', 'C12345', 'FDA: A=B',           'IMP',                  '1 Day',                  'Admin 1',              'Admin Desc 1',           'Admin Label 1',    'C34567',          '12 mg',            'C65432',               'Dur desc 1',                     'False',                       '',                                  '14 %'                         ], 
+    [ '',      '',            '',            '',                            '',                          '',       '',                   '',                     '',                       'Admin 2',              'Admin Desc 2',           'Admin Label 1',    'C34567',          '1 mg',             'C65432',               'Dur desc 1',                     'False',                       '',                                  '10 m'                         ], 
+    [ 'Int 2', 'Int Desc 2',  'Int Label 2', 'SPONSOR: C=D',                'Placebo',                   'C12345', 'FDA: A=B',           'NIMP (AxMP)',          '3 Weeks',                'Admin 3',              'Admin Desc 3',           'Admin Label 1',    'C34567',          '100 mg',           'C65432',               'Dur desc 1',                     'False',                       '',                                  '12 C'                         ], 
+    [ 'Int 3', 'Int Desc 3',  'Int Label 3', 'SPONSOR: E=F, SPONSOR: G=H',  'Rescue Medicine',           'C12345', 'FDA: A=B',           'IMP',                  '4 Years',                'Admin 4',              'Admin Desc 4',           'Admin Label 1',    'C34567',          '500 mg',           'C65432',               'Dur desc 1',                     'False',                       '',                                  '12 F'                         ], 
+    [ '',      '',            '',            '',                            '',                          '',       '',                   '',                     '',                       'Admin 5',              'Admin Desc 5',           'Admin Label 1',    'C34567',          '1 mg',             'C65432',               'Dur desc 1',                     'False',                       '',                                  '15 in'                        ], 
   ]
+
+
+
+
   mock_read = mocker.patch("pandas.read_excel")
   mock_read.return_value = pd.DataFrame(data, columns=COLUMNS)
   interventions = StudyDesignInterventionSheet("")
+  #dump_json({'items': [json.loads(x.to_json()) for x in interventions.items]})
   for index, item in enumerate(interventions.items):
     result = json.loads(item.to_json())
     assert result == expected["items"][index]
