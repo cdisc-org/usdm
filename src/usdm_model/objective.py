22a0e6d321e43c0e02dc9fd55dfb904348284459
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2024-03-22 11:45:48 +0100

    Objective level change

--- src/usdm_model/objective.py
+++ src/usdm_model/objective.py
@@ -4,6 +4,6 @@ from .code import Code
 from .endpoint import Endpoint
 
 class Objective(SyntaxTemplate):
-  level: Union[Code, None] = None
+  level: Code
   endpoints: List[Endpoint] = []
   instanceType: Literal['Objective']
