644d6ee5aee72a5ce37bb7323cf1d58026f0fb91
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2024-04-18 17:52:52 +0200

    Add in characterisitics

--- src/usdm_excel/study_design_population_sheet/study_design_population_sheet.py
+++ src/usdm_excel/study_design_population_sheet/study_design_population_sheet.py
@@ -1,6 +1,7 @@
-import traceback
 from usdm_excel.base_sheet import BaseSheet
 from usdm_model.population_definition import StudyDesignPopulation, StudyCohort
+from usdm_model.range import Range
+from usdm_model.characteristic import Characteristic
 from usdm_excel.globals import Globals
 
 class StudyDesignPopulationSheet(BaseSheet):
@@ -11,7 +12,7 @@ class StudyDesignPopulationSheet(BaseSheet):
     try:
       super().__init__(file_path=file_path, globals=globals, sheet_name=self.SHEET_NAME)
       self.population = None
-      cohorts = []
+      self._cohorts = []
       for index, row in self.sheet.iterrows():
         level = self.read_cell_by_name(index, 'level')
         name = self.read_cell_by_name(index, 'name')
@@ -21,14 +22,14 @@ class StudyDesignPopulationSheet(BaseSheet):
         recruit_number = self.read_range_cell_by_name(index, "plannedEnrollmentNumber", require_units=False, allow_empty=True)
         planned_age = self.read_range_cell_by_name(index, "plannedAge", require_units=True, allow_empty=True)
         healthy = self.read_boolean_cell_by_name(index, 'includesHealthySubjects', must_be_present=False)
+        characteristics = self.read_cell_multiple_by_name(index, 'characteristics', must_be_present=False)
         codes = self._build_codes(row, index)
         if level.upper() == "MAIN":
           self.population = self._study_population(name, description, label, recruit_number, required_number, planned_age, healthy, codes)
         else:
-          cohort = self._study_cohort(name, description, label, recruit_number, required_number, planned_age, healthy, codes)
-          cohorts.append(cohort)
+          cohort = self._study_cohort(name, description, label, recruit_number, required_number, planned_age, healthy, codes, characteristics)
       if self.population: 
-        self.population.cohorts = cohorts
+        self.population.cohorts = self._cohorts
       else:
         self._general_error(f"Not main study population detected")
     except Exception as e:
@@ -38,41 +39,47 @@ class StudyDesignPopulationSheet(BaseSheet):
     code = self.read_cdisc_klass_attribute_cell_by_name('StudyDesignPopulation', "plannedSexOfParticipants", index, "plannedSexOfParticipants", allow_empty=True)
     return [code] if code else []
 
-  def _study_population(self, name, description, label, recruit_number, required_number, planned_age, healthy, codes):    
-    try:
-      item = StudyDesignPopulation(id=self.globals.id_manager.build_id(StudyDesignPopulation),
-        name=name,
-        description=description,
-        label=label,
-        includesHealthySubjects=healthy,
-        plannedEnrollmentNumber=recruit_number,
-        plannedCompletionNumber=required_number,
-        plannedAge=planned_age,
-        plannedSex=codes
-      )
-    except Exception as e:
-      self._general_exception(f"Failed to create StudyDesignPopulation object", e)
-      return None
-    else:
+  def _study_population(self, name: str, description: str, label: str, recruit_number: Range, required_number: Range, planned_age: Range, healthy: bool, codes: list) -> StudyDesignPopulation:    
+    params = {
+      'name': name,
+      'description': description,
+      'label': label,
+      'includesHealthySubjects': healthy,
+      'plannedEnrollmentNumber': recruit_number,
+      'plannedCompletionNumber': required_number,
+      'plannedAge': planned_age,
+      'plannedSex': codes
+    }
+    item = self.create_object(StudyDesignPopulation, params)
+    if item:
       self.globals.cross_references.add(name, item)
-      return item
+    return item
 
-  def _study_cohort(self, name, description, label, recruit_number, required_number, planned_age, healthy, codes):    
-    try:
-      item = StudyCohort(id=self.globals.id_manager.build_id(StudyCohort),
-        name=name,
-        description=description,
-        label=label,
-        includesHealthySubjects=healthy,
-        plannedEnrollmentNumber=recruit_number,
-        plannedCompletionNumber=required_number,
-        plannedAge=planned_age,
-        plannedSex=codes,
-        characteristics=[]
-      )
-    except Exception as e:
-      self._general_exception(f"Failed to create StudyCohort object", e)
-      return None
-    else:
+  def _study_cohort(self, name: str, description: str, label: str, recruit_number: Range, required_number: Range, planned_age: Range, healthy: bool, codes: list, characteristics: list) -> StudyCohort:    
+    characteristic_refs = self._resolve_characteristics(characteristics)
+    params = {  
+      'name': name,
+      'description': description,
+      'label': label,
+      'includesHealthySubjects': healthy,
+      'plannedEnrollmentNumber': recruit_number,
+      'plannedCompletionNumber': required_number,
+      'plannedAge': planned_age,
+      'plannedSex': codes,
+      'characteristics': characteristic_refs
+    }
+    item = self.create_object(StudyCohort, params)
+    if item:
       self.globals.cross_references.add(name, item)
-      return item
+      self._cohorts.append(item)
+    return item
+      
+  def _resolve_characteristics(self, names):
+    results = []
+    for name in names:
+      object = self.globals.cross_references.get(Characteristic, name)
+      if object:
+        results.append(object.id)
+      else:
+        self._general_warning(f"Characterisitc '{name}' not found")
+    return results
