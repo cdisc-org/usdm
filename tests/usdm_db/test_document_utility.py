e78ce992d9427a9bca9bad62481ab3dc8fb8b0c4
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2024-04-18 19:17:46 +0200

    Add tests

---
+++ tests/usdm_db/test_document_utility.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+from usdm_db.document.utility import *
+from usdm_db.errors_and_logging.errors_and_logging import ErrorsAndLogging
+
+class ClassA(BaseModel):
+  id: str
+
+errors_and_logging = ErrorsAndLogging()
+
+def test_usdm_reference():
+  a = ClassA(id='123')
+  ref = usdm_reference(a, 'attribute')
+  assert ref == '<usdm:ref klass="ClassA" id="123" attribute="attribute"></usdm:ref>'
+  
+
+def test_get_soup():
+  result = get_soup('', errors_and_logging)
+  assert result == ''
