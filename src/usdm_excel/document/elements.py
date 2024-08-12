d9ea50bdc73859422cf9afa1e33a3774fce69522
Dave Iberson-Hurst <daveih@dih-m1-mini.home>
2024-08-12 12:19:50 +0200

    Fix tests

--- src/usdm_excel/document/elements.py
+++ src/usdm_excel/document/elements.py
@@ -2,54 +2,58 @@ from usdm_excel.base_sheet import BaseSheet
 
 class Elements():
 
-  def __init__(self, parent: BaseSheet, study):
+  def __init__(self, parent: BaseSheet, study, template_name: str):
     super().__init__()
-    self.parent = parent
-    self.study = study
-    self.study_version = study.versions[0]
-    self.study_design = self.study_version.studyDesigns[0]
-    self.protocol_document_version = self.study.documentedBy.versions[0]
-    self.methods = [func for func in dir(self.__class__) if callable(getattr(self.__class__, func)) and not func.startswith("_")]
+    self._parent = parent
+    self._study = study
+    # self._study_version = study.versions[0]
+    # self.study_design = self._study_version.studyDesigns[0]
+    # self._document_version = self.study.documentedBy.versions[0]
+    # self.methods = [func for func in dir(self.__class__) if callable(getattr(self.__class__, func)) and not func.startswith("_")]
+    self._study_version = self._study.versions[0]
+    self._study_design = self._study_version.studyDesigns[0]
+    self._document = self._study.document_by_template_name(template_name)
+    self._document_version = self._document.versions[0]
 
   def valid_method(self, name):
     return name in self.methods
 
   def study_phase(self):
-    phase = self.study_version.phase()
+    phase = self._study_version.phase()
     results = [{'instance': phase, 'klass': 'Code', 'attribute': 'decode'}]
     return self._set_of_references(results)
 
   def study_short_title(self):
-    title = self.study_version.get_title('Brief Study Title')
+    title = self._study_version.get_title('Brief Study Title')
     results = [{'instance': title, 'klass': 'StudyTitle', 'attribute': 'text'}] if title else []
     return self._set_of_references(results)
 
   def study_full_title(self):
-    title = self.study_version.get_title('Official Study Title')
+    title = self._study_version.get_title('Official Study Title')
     results = [{'instance': title, 'klass': 'StudyTitle', 'attribute': 'text'}] if title else []
     return self._set_of_references(results)
 
   def study_acronym(self):
-    title = self.study_version.get_title('Study Acronym')
+    title = self._study_version.get_title('Study Acronym')
     results = [{'instance': title, 'klass': 'StudyTitle', 'attribute': 'text'}] if title else []
     return self._set_of_references(results)
 
   def study_rationale(self):
-    results = [{'instance': self.study_version, 'klass': 'StudyVersion', 'attribute': 'rationale'}]
+    results = [{'instance': self._study_version, 'klass': 'StudyVersion', 'attribute': 'rationale'}]
     return self._set_of_references(results)
 
   def study_version_identifier(self):
-    results = [{'instance': self.study_version, 'klass': 'StudyVersion', 'attribute': 'versionIdentifier'}]
+    results = [{'instance': self._study_version, 'klass': 'StudyVersion', 'attribute': 'versionIdentifier'}]
     return self._set_of_references(results)
 
   def study_identifier(self):
-    identifier = self.study_version.sponsor_identifier()
+    identifier = self._study_version.sponsor_identifier()
     results = [{'instance': identifier, 'klass': 'StudyIdentifier', 'attribute': 'studyIdentifier'}]
     return self._set_of_references(results)
 
   def study_regulatory_identifiers(self):
     results = []
-    identifiers = self.study_version.studyIdentifiers
+    identifiers = self._study_version.studyIdentifiers
     for identifier in identifiers:
       if identifier.studyIdentifierScope.organizationType.code == 'C188863' or identifier.studyIdentifierScope.organizationType.code == 'C93453':
         item = {'instance': identifier, 'klass': 'StudyIdentifier', 'attribute': 'studyIdentifier'}
@@ -57,7 +61,7 @@ class Elements():
     return self._set_of_references(results)
 
   def study_date(self):
-    dates = self.protocol_document_version.dateValues
+    dates = self._document_version.dateValues
     for date in dates:
       if date.type.code == 'C99903x1':
         results = [{'instance': date, 'klass': 'GovernanceDate', 'attribute': 'dateValue'}]
@@ -65,7 +69,7 @@ class Elements():
     return []
   
   def approval_date(self):
-    dates = self.study_version.dateValues
+    dates = self._study_version.dateValues
     for date in dates:
       if date.type.code == 'C132352':
         results = [{'instance': date, 'klass': 'GovernanceDate', 'attribute': 'dateValue'}]
@@ -73,7 +77,7 @@ class Elements():
     return []
 
   def organization_name_and_address(self):
-    identifier = self.study_version.sponsor_identifier()
+    identifier = self._study_version.sponsor_identifier()
     results = [
       {'instance': identifier.studyIdentifierScope, 'klass': 'Organization', 'attribute': 'name'},
       {'instance': identifier.studyIdentifierScope.legalAddress, 'klass': 'Address', 'attribute': 'text'},
@@ -81,27 +85,27 @@ class Elements():
     return self._set_of_references(results)
 
   def organization_address(self):
-    identifier = self.study_version.sponsor_identifier()
+    identifier = self._study_version.sponsor_identifier()
     results = [
       {'instance': identifier.studyIdentifierScope.legalAddress, 'klass': 'Address', 'attribute': 'text'},
     ]
     return self._set_of_references(results)
 
   def organization_name(self):
-    identifier = self.study_version.sponsor_identifier()
+    identifier = self._study_version.sponsor_identifier()
     results = [
       {'instance': identifier.studyIdentifierScope, 'klass': 'Organization', 'attribute': 'name'},
     ]
     return self._set_of_references(results)
 
   def amendment(self):
-    amendments = self.study_version.amendments
+    amendments = self._study_version.amendments
     results = [{'instance': amendments[-1], 'klass': 'StudyAmendment', 'attribute': 'number'}]
     return self._set_of_references(results)
 
   def amendment_scopes(self):
     results = []
-    amendment = self.study_version.amendments[-1]
+    amendment = self._study_version.amendments[-1]
     for item in amendment.enrollments:
       if item.type.code == "C68846":
         results = [{'instance': item.type, 'klass': 'Code', 'attribute': 'decode'}]
@@ -115,7 +119,7 @@ class Elements():
     return ""
 
   # def _sponsor_identifier(self):
-  #   identifiers = self.study_version.studyIdentifiers
+  #   identifiers = self._study_version.studyIdentifiers
   #   for identifier in identifiers:
   #     if identifier.studyIdentifierScope.organizationType.code == 'C70793':
   #       return identifier
@@ -128,7 +132,7 @@ class Elements():
       return ""
 
   # def _get_title(self, title_type):
-  #   for title in self.study_version.titles:
+  #   for title in self._study_version.titles:
   #     if title.type.decode == title_type:
   #       return title
   #   return None
--- tests/test_document_elements.py
+++ tests/test_document_elements.py
@@ -2,10 +2,10 @@ from usdm_excel.document.elements import Elements
 
 def test_organization_address(mocker, globals, factory, minimal):
   sheet = factory.base_sheet(mocker)
-  elements = Elements(sheet, minimal.study)
+  elements = Elements(sheet, minimal.study, 'sponsor')
   assert elements.organization_address() == '<usdm:ref klass="Address" id="Address_1" attribute="text"/>'
 
 def test_organization_name(mocker, globals, factory, minimal):
   sheet = factory.base_sheet(mocker)
-  elements = Elements(sheet, minimal.study)
+  elements = Elements(sheet, minimal.study, 'sponsor')
   assert elements.organization_name() == '<usdm:ref klass="Organization" id="Organization_1" attribute="name"/>'
