d656b5f1d5ffe8de7bbeac0a76f73185401df616
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2025-02-04 15:26:37 +0100

    Update version for next release

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -5,7 +5,7 @@ from usdm_db import USDMDb
 from bs4 import BeautifulSoup
 from uuid import UUID
 
-SAVE_ALL = True
+SAVE_ALL = False
 
 def save_error_csv(file, contents):
   writer = csv.DictWriter(file, fieldnames=['sheet','row','column','message','level'])
