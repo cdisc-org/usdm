fc177d8ae4a53d14f60a4c11260d0ac1fc853372
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2025-07-16 14:41:44 +0200

    Add new test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -144,7 +144,7 @@ def run_test_fhir(filename, mocker, save=False):
     usdm.from_excel(f"tests/integration_test_files/{filename}.xlsx")
     result = usdm.to_fhir("sponsor")
 
-    #print(f"RESULT: {result}")
+    # print(f"RESULT: {result}")
 
     if save or SAVE_ALL:
         with open(
@@ -359,3 +359,7 @@ def test_study_devices_1(mocker):
 
 def test_time_duration_1(mocker):
     run_test("timeline_duration", mocker)
+
+
+def test_minimum(mocker):
+    run_test("minimum", mocker)
